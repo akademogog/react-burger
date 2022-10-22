eb28f10b435ef838af71c59b88a7a19bc1613f5d
Alexandr Bolotov <alexandr.29.07.bolotov@gmail.com>
2022-10-22 17:58:55 +0300

    sprint-2/step-2

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,11 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { burgerIngredientsReduser } from "./reducers/burgerIngredientsRedusers";
 
-export const store = createStore(burgerIngredientsReduser);
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose; 
+
+const enhancer = composeEnhancers();
+
+export const store = createStore(burgerIngredientsReduser, enhancer);
